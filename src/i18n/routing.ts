d1753a8daa253fa59f1e97ac3b614e48dc7abf29
Yuri Kravcenko <1843084+1ur11@users.noreply.github.com>
2025-08-22 01:13:05 +0300

    disable localeDetection

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -6,4 +6,7 @@ export const routing = defineRouting({
 
   // Used when no locale matches
   defaultLocale: 'de',
+  
+  // Disable automatic locale detection to force default locale
+  localeDetection: false,
 });
