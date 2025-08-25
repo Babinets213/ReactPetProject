0509f39fa97c5965079ac1a1b890eac23f57683a
vdevvv <vovakrysaa@gmail.com>
2025-08-25 18:53:17 +0300

    fix icons

--- src/components/icons/MailIcon.tsx
+++ src/components/icons/MailIcon.tsx
@@ -9,20 +9,20 @@ export default function MailIcon() {
       viewBox="0 0 32 33"
       fill="none"
     >
-      <g opacity="0.7" clip-path="url(#clip0_225_489)">
+      <g opacity="0.7" clipPath="url(#clip0_225_489)">
         <path
           d="M4 7.5H28V24.5C28 24.7652 27.8946 25.0196 27.7071 25.2071C27.5196 25.3946 27.2652 25.5 27 25.5H5C4.73478 25.5 4.48043 25.3946 4.29289 25.2071C4.10536 25.0196 4 24.7652 4 24.5V7.5Z"
           stroke="#1A2434"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M28 7.5L16 18.5L4 7.5"
           stroke="#1A2434"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
