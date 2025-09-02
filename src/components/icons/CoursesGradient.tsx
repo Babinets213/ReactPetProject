697401d86514409f76cab9034fc2c8dddf08d195
vdevvv <vovakrysaa@gmail.com>
2025-09-02 20:55:01 +0300

    finished all courses

---
+++ src/components/icons/CoursesGradient.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+export default function CoursesGradient() {
+  return (
+    <svg
+      className="w-full"
+      overflow="visible"
+      height="1942"
+      viewBox="0 0 1597 1942"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_f_946_5667)">
+        <ellipse
+          cx="249"
+          cy="963"
+          rx="1118"
+          ry="749"
+          fill="url(#paint0_radial_946_5667)"
+          opacity="0.35"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_f_946_5667"
+          x="-1099"
+          y="-16"
+          width="2696"
+          height="1958"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feGaussianBlur
+            stdDeviation="115"
+            result="effect1_foregroundBlur_946_5667"
+          />
+        </filter>
+        <radialGradient
+          id="paint0_radial_946_5667"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientTransform="matrix(5239.58 7248.8 -10820 40740.4 -1975.88 -3531)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.175497" stop-color="#A0F384" />
+          <stop offset="0.275566" stop-color="#9BFFA5" />
+          <stop offset="0.49892" stop-color="#D4F3FF" />
+          <stop offset="0.591346" stop-color="#C9EDEF" />
+          <stop offset="0.701923" stop-color="#CAFAE9" />
+        </radialGradient>
+      </defs>
+    </svg>
+  );
+}
