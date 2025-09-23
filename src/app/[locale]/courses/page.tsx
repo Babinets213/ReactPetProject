b486c290580e30168913408dfdbe4cdde88702ec
Yuri Kravcenko <1843084+1ur11@users.noreply.github.com>
2025-09-24 00:37:49 +0300

    update courses page

--- src/app/[locale]/courses/page.tsx
+++ src/app/[locale]/courses/page.tsx
@@ -5,20 +5,36 @@ import { CoursesService } from "@/services/courses";
 import OpenedCourseCard from "@/components/OpenedCourseCard";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { inter700 } from "@/styles/fonts";
+import { inter400, inter600, inter700 } from "@/styles/fonts";
 import { useCart } from "@/context/CartContext";
 import { ApiCourse } from "@/types/courses";
+// import SmallCourseCards from "@/components/SmallCourseCards";
+import { useTranslations } from "next-intl";
+import CoursesGradient from "@/components/icons/CoursesGradient";
+import Button from "@/components/ui/Button";
+import router from "next/router";
 
 export default function CoursesPage() {
   const [publicCourses, setPublicCourses] = useState<ApiCourse[]>([]);
   const [error, setError] = useState<string | null>(null);
   const { cart, addToCart, deleteFromCart } = useCart();
+  const t = useTranslations("AllCoursesPage");
 
   useEffect(() => {
     CoursesService.fetchPublicCourses()
       .then(setPublicCourses)
       .catch(() => setError("Failed to fetch courses"));
   }, []);
+
+  // const handleAddCard = function (course: CartItem) {
+  //   addToCart(course);
+  // };
+
+  // const handleDeleteCard = function (courseId: string) {
+  //   deleteFromCart(courseId);
+  // };
+
+  const totalCartPrice = cart.reduce((red, cur) => red, 0);
 
   return (
     <div className="relative min-h-screen">
@@ -44,7 +60,64 @@ export default function CoursesPage() {
             ))}
           </div>
         )}
+        {/* <SmallCourseCards
+          title={t("course.professional.title")}
+          onHandleDeleteCard={handleDeleteCard}
+          onHandleAddCard={handleAddCard}
+          courses={professionalBlock}
+          cart={cart}
+          className="mb-15"
+        />
+
+        <SmallCourseCards
+          title={t("course.expert.title")}
+          onHandleDeleteCard={handleDeleteCard}
+          onHandleAddCard={handleAddCard}
+          courses={expertBlock}
+          cart={cart}
+          className="mb-21"
+        /> */}
       </main>
+      {cart.length > 0 && (
+        <div className="fixed right-0 bottom-0 left-0 z-50 border-t border-t-[#F1F1F3] bg-white shadow-md sm:px-5 lg:px-10 2xl:px-60">
+          <div className="flex items-center justify-between">
+            <p
+              className={`${inter700.className} text-[23px] leading-[120%] text-[#2A354F]`}
+            >
+              You have selected {cart.length} courses
+            </p>
+
+            <div className="flex items-center">
+              <div className="mr-7 flex w-50 flex-col items-start justify-center py-4">
+                <span
+                  className={`${inter400.className} max-w-[380px] items-center self-stretch text-base leading-[120%] text-[#687083]`}
+                >
+                  Total
+                </span>
+                <span
+                  className={`${inter600.className} text-lg leading-[120%] text-[#2A354F]`}
+                >
+                  {totalCartPrice}.00 CHF
+                </span>
+              </div>
+              <Button content="text" btnType="primary" size="normal">
+                Proceed to Checkout
+              </Button>
+            </div>
+            <Button
+              onClick={() => router.replace("/cart")}
+              content="text"
+              btnType="primary"
+              size="normal"
+            >
+              Proceed to Checkout
+            </Button>
+          </div>
+        </div>
+      )}
+      <div className="absolute top-100 -left-100 -z-100">
+        <CoursesGradient />
+      </div>
       <Footer />
     </div>
   );
